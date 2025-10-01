6dc71ff543d294560b370038d22ce813bc607de9
MrAsuintupa <juho.asuintupa@gmail.com>
2025-10-01 09:34:54 +0300

    Tehtävä 5. Madon pituuden kasvatus

--- matopeli.py
+++ matopeli.py
@@ -70,9 +70,10 @@ class SnakeGame(QGraphicsView):
         self.snake.insert(0, new_head)      
         if new_head == self.food:
             self.score += 1
-            self.food = self.add_food()
+            self.food = self.spawn_food()
         else:
             self.snake.pop()
+        
             
 
         self.print_game()
